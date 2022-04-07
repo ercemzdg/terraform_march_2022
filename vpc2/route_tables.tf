454d9a4bad2077b5efe7394001b97e442f1ed033
ercemzdg <ercemzdg@gmail.com>
2022-04-07 22:24:33 +0000

    vpc task on progress

--- vpc2/route_tables.tf
+++ vpc2/route_tables.tf
@@ -1,5 +1,26 @@
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.task_vpc.id
-    
-  
+  vpc_id = aws_vpc.task_vpc.id
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.task_vpc.id
+  route {
+    cidr_block = "10.0.0.0/16"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+
+  }
+
+}
+resource "aws_route_table_association" "public"{
+  subnet_id = aws_subnet.public[2].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private[2].id
+  route_table_id = aws_route_table.private.id
 }
