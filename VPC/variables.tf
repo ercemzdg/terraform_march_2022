197f11edb0c275c6d7a89a5c0dcd6f8a04b05e24
ercemzdg <ercemzdg@gmail.com>
2022-04-06 23:59:27 -0500

    vpc updated

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -7,13 +7,20 @@ variable "vpc_cidr" {
 
     default = "10.0.0.0/16" 
 }
-variable "subnets_cidr" {
+variable "public_subnets_cidr" {
 
     type = list
     default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] 
     }
 
-    variable "az_subnets" {
+
+variable "private_subnets_cidr" {
+
+    type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"] 
+    }
+
+variable "az_subnets" {
     type = list
     default = ["us-east-1a", "us-east-1b", "us-east-1c"] 
     }
