ae83f49924a24e095f6c32273c193824cd8a4b16
ercemzdg <ercemzdg@gmail.com>
2022-04-12 02:19:04 +0000

    session-7 added

--- session-7/backend/data_sources.tf
+++ session-7/backend/data_sources.tf
@@ -2,14 +2,18 @@
 # Data source only fatches the data from outputs.
 
 data "terraform_remote_state" "backend" {
-  backend = "s3"
-  config = {
-    bucket = "s3-terraform-session"
-    key    = "session-7/backend/terraform.tfstate"
-    region = "us-east-1"
-  }
+    backend = "s3"
+    config = {
+        bucket = "s3-terraform-session"
+        key = "session-7/backend/terraform.tfstate"
+        region = "us-east-1"
+    }
+}
 
-}           
 output "password" {
     value = data.terraform_remote_state.backend.outputs.db_password
+}
+
+output "username" {
+    value = data.terraform_remote_state.backend.outputs.db_user
 }
