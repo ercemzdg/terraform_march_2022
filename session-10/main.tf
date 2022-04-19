6ba3673837be9e7ce7ceebc7b175b78f50d5a1c7
ercemzdg <ercemzdg@gmail.com>
2022-04-19 00:45:40 +0000

    session-10 added

--- session-10/main.tf
+++ session-10/main.tf
@@ -3,11 +3,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.0"
 
-// Variables 
+  // Variables 
   name = "aws-session-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a","us-east-1b", "us-east-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.103.0/24"]
 
@@ -15,11 +15,21 @@ module "vpc" {
   enable_vpn_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 module "s3" {
-  source = "github.com/ercemzdg/terraform_march_2022/module/s3"
-  env = "dev"
+  source = "github.com/ercemzdg/terraform_march_2022//module//s3"
+  env    = "dev"
 }
+
+# ?ref=version!!!!
+// github.com = domain name
+// aKumoSolutions = organization
+// terraform-march-2022 = repository
+// tree = branches
+// main = default branches
+// modules = subdirectory
+// s3 = subdirectory
+
